d9ee16ee7f5f20220b7c0e70b0bd001cacc8aaaa
Aditya <147301021+adityajha2005@users.noreply.github.com>
2024-09-28 19:29:05 +0530

    first commit

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React from "react";
+import { Home, Info, Code, TerminalIcon, Download } from "lucide-react"; // Import the Download icon
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ModeToggle } from "../ui/darkmode";
+
+const navbarItems = [
+    {
+        name: "Home",
+        href: "/",
+        icon: Home,
+    },
+    {
+        name: "Projects",
+        href: "/projects",
+        icon: TerminalIcon,
+    },
+    {
+        name: "Skills",
+        href: "/skills",
+        icon: Code,
+    },
+    {
+        name: "About",
+        href: "/about",
+        icon: Info,
+    },
+];
+
+const Navbar = () => {
+    const pathname = usePathname();
+    return (
+        <nav className="z-50 lg:sticky max-w-[90%] m-auto lg:top-0">
+            {/* iPad & Desktop Navbar */}
+            <section className="flex w-full justify-between items-center m-auto p-2">
+                <Link href="/" className="uppercase text-3xl font-bold tracking-widest relative">
+                    AJ<span className="text-blue-700 font-bold text-8xl absolute -bottom-2">.</span>{" "}
+                </Link>
+                <div className="hidden lg:flex gap-5 min-w-96 justify-center items-center backdrop-blur-lg p-2">
+                    {navbarItems.map((item) => {
+                        const isActive = (pathname.includes(item.href) && item.href.length > 1) || pathname === item.href;
+                        return (
+                            <Link
+                                href={item.href}
+                                key={item.name}
+                                className={`${isActive ? "text-blue-700" : "text-gray-800 dark:text-gray-200"} flex items-center justify-center gap-2 uppercase hover:text-blue-500 duration-200 ease-linear w-36 font-medium text-center`}
+                            >
+                                <item.icon /> {item.name}
+                            </Link>
+                        );
+                    })}
+                </div>
+                <div className="flex items-center gap-4">
+                    <Link
+                    // href="Aditya_resume.pdf"
+                        href="https://drive.google.com/file/d/1FMyl2mj4A5JlUQhOj_BsCKnpiuyf2iUo/view?usp=sharing" // Replace with the actual link to your resume
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-800 dark:text-gray-200 hover:text-blue-500 duration-200 ease-linear font-medium uppercase flex items-center gap-2"
+                    >
+                        <Download className="w-4 h-4" />
+                         Resume
+                    </Link>
+                    <ModeToggle />
+                </div>
+            </section>
+            {/* Mobile Navbar */}
+            <section className="fixed z-50 md:w-[30rem] md:m-auto lg:hidden bottom-10 left-0 right-0 flex flex-wrap justify-center w-full">
+                <div className="flex gap-3 flex-wrap items-center w-[60%] justify-between p-2 bg-black/20 dark:bg-white/5 backdrop-blur-lg">
+                    {navbarItems.map((item) => {
+                        const isActive = (pathname.includes(item.href) && item.href.length > 1) || pathname === item.href;
+                        return (
+                            <Link key={item.name} href={item.href} className={`${isActive ? "text-blue-700" : "text-gray-800 dark:text-gray-200"} p-2`}>
+                                <item.icon />
+                            </Link>
+                        );
+                    })}
+                </div>
+            </section>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/words.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FlipWords } from "./ui/words";
+
+export function FlipWordsDemo() {
+  const words = [ "Competitive\u00A0Programmer", 
+    "MERN\u00A0Developer",
+    "Web-3\u00A0Enthusiast",
+    // "OpenSource\u00A0Contributor",
+    "ML\u00A0Researcher",
+    "Coder",
+  // "Freelancer"
+]
+
+  return (
+    <div className="justify-center items-center px-4">
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 mt-5 mb-3">
+      I am a 
+        <FlipWords words={words} /> <br />
+       
+      </div>
+    </div>
+  );
+}
