b2a806b9581b743d6fd2bef4515e19b35c6c4a78
Aditya <147301021+adityajha2005@users.noreply.github.com>
2024-10-13 15:17:19 +0530

    updated Projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,6 +12,58 @@ export default function Component() {
         <div className="space-y-6">
 
           <div className="grid gap-5 sm:grid-cols-1 lg:grid-cols-2">
+          <CardContainer className="inter-var">
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+        EcoZap - Blockchain-powered CO2 tracking system
+        </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  HTML, CSS, Javascript, Typescript, Solidity, Python, Jupyter Notebook
+                </CardItem>
+                <CardItem
+                  translateZ="100"
+                  rotateX={0}
+                  rotateZ={-3}
+                  className="w-full mt-4"
+                >
+                  <Image
+                    src="/assests/EcoZap.png"
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <CardItem
+                    translateZ={20}
+                    translateX={-40}
+                    as="a"
+                    href="https://eco-zap.vercel.app/"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Live Link →
+                  </CardItem>
+
+                  <CardItem
+                    translateZ={20}
+                    translateX={40}
+                    as="a"
+                    href="https://github.com/adityajha2005/EcoZap"
+                    className="flex items-center px-4 py-2 text-white text-xl font-bold"
+                  >
+                    <GitHubLogoIcon className="w-8 h-8" />
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
             <CardContainer className="inter-var">
               <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
@@ -167,7 +219,7 @@ export default function Component() {
                 </div>
               </CardBody>
             </CardContainer>
-            <CardContainer className="inter-var">
+            {/* <CardContainer className="inter-var">
               <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
                   translateZ="50"
@@ -217,7 +269,7 @@ export default function Component() {
                   </CardItem>
                 </div>
               </CardBody>
-            </CardContainer>
+            </CardContainer> */}
             {/* <CardContainer className="inter-var">
               <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
