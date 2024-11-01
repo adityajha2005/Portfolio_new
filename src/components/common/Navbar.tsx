5c925a4f02f938d597fbddbd77f8ea9cb78e1cbb
Aditya <147301021+adityajha2005@users.noreply.github.com>
2024-11-01 17:31:58 +0530

    updated mobile navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -73,8 +73,8 @@ const Navbar = () => {
                 </div>
             </section>
             {/* Mobile Navbar */}
-            <section className="fixed z-50 md:w-[30rem] md:m-auto lg:hidden bottom-10 left-0 right-0 flex flex-wrap justify-center w-full">
-                <div className="flex gap-3 flex-wrap items-center w-[60%] justify-between p-2 bg-black/20 dark:bg-white/5 backdrop-blur-lg">
+            <section className="fixed z-50 md:w-[30rem] md:m-auto lg:hidden bottom-10 left-0 right-0 flex flex-wrap justify-center items-center w-full">
+                <div className="flex gap-1 flex-wrap items-center w-[6  0%] justify-between p-2 bg-black/20 dark:bg-white/5 backdrop-blur-lg">
                     {navbarItems.map((item) => {
                         const isActive = (pathname.includes(item.href) && item.href.length > 1) || pathname === item.href;
                         return (
