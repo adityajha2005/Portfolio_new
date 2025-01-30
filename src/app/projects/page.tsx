3ef8d5128c7c184043303a2f1c288153487a37fd
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-01-30 14:24:43 +0530

    added careinsight

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,6 +13,63 @@ export default function Component() {
 
           <div className="grid gap-5 sm:grid-cols-1 lg:grid-cols-2">
           <CardContainer className="inter-var">
+          <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}>
+
+              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+        CareInsight
+        </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  Next.js, TypeScript, Firebase, Python, NLP (Hugging Face, Cohere API), Clerk, Google Gemini API.                </CardItem>
+                <CardItem
+                  translateZ="100"
+                  rotateX={0}
+                  rotateZ={-3}
+                  className="w-full mt-4"
+                >
+                  <Image
+                  // public\assests\careinsightimg1.png
+                    src="/assests/careinsightimg1.png"
+                    height="1000"
+                    width="1000"
+                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+                <div className="flex justify-between items-center mt-20">
+                  <CardItem
+                    translateZ={20}
+                    translateX={-40}
+                    as="a"
+                    href="https://www.youtube.com/watch?v=uKxrZahZoB8"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Yt Link (not deployed) →
+                  </CardItem>
+
+                  <CardItem
+                    translateZ={20}
+                    translateX={40}
+                    as="a"
+                    href="https://github.com/adityajha2005/careinsight"
+                    className="flex items-center px-4 py-2 text-white text-xl font-bold"
+                  >
+                    <GitHubLogoIcon className="w-8 h-8" />
+                  </CardItem>
+                </div>
+              </CardBody>
+              </motion.div>
+
+            </CardContainer>
+          <CardContainer className="inter-var">
             <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}
                                   transition={{ duration: 0.5, ease: "easeOut" }}>
 
