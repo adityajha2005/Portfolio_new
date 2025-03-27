5ca131ddaf08c112f2448d9e643216ed14e5e9a5
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-28 00:43:01 +0530

    updated resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ const Home = () => {
                 whileTap={{ scale: 0.95 }}
               >
                 <Link
-                  href="https://drive.google.com/file/d/1eOq8BJ9YyOZhWlJPXoOFlXETMBa7TNQz/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1QWm4nkNHkf4p7TrN_LQph90rkJn2WmMh/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center justify-center text-sm font-medium bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors duration-200"
